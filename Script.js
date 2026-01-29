b5550706fb93b89901bb3b80745277c6db53cf5e
WHENMANANCODES <mananjain564@gmail.com>
2026-01-29 23:35:55 +0530

    Added pause and play function on some songs

--- Script.js
+++ Script.js
@@ -1,10 +1,36 @@
 const cards = document.querySelectorAll(".card"); // first card only
 const audio = document.getElementById("audio");
 
+let currsong = null;
 cards.forEach(cardio=>{
     cardio.addEventListener("click", ()=>{
+    const icon = cardio.querySelector("i");
     const Songsrc = cardio.dataset.song;
-    audio.src = Songsrc;
-    audio.play();
+    //same song clicked
+    if(currsong === Songsrc){
+        if(audio.paused){
+            icon.classList.replace("fa-play","fa-pause");
+            icon.classList.add("pausebtn");
+            audio.play();
+            
+           
+        }
+        else{
+            icon.classList.replace("fa-pause","fa-play");
+            icon.classList.remove("pausebtn");
+            audio.pause();
+            
+        }
+    } 
+   // different song clicked
+    else{
+        currsong = Songsrc;
+        audio.src = Songsrc;
+        icon.classList.replace("fa-play","fa-pause");
+        icon.classList.add("pausebtn");
+        audio.play();
+      
+    }
+
 })
 })
